938ffe3102d29a41ad089eeea0a873ea4b7eaecd
Tony <xaj@gmail.com>
2021-04-07 15:37:28 +0800

    创建组件并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h1>头条发布系统</h1>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载element组件库
+import ElementUI from 'element-ui'
+
+// 加载elemen组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
 // 加载全局变量
 import './styles/index.less'
+
+// 全局注册element组件库
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
